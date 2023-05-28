78189946cce341a9149524847952639529eb7a97
AntonParker18 <55868272+AntonParker18@users.noreply.github.com>
2023-05-28 19:54:21 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
+import {HashRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Shops from './components/Shops/Shops'
 import ShoppingCart from './components/ShoppingCart/ShoppingCart'
